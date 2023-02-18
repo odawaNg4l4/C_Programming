363fda03d2eb3ccea7cba95a5dfd98ab47bc0cfa
odawaNg4l4 <odawangala99@gmail.com>
2023-02-18 22:12:55 +0300

    c

---
+++ Minutes to days and years..c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    double min;
+    double hrs;
+    printf("Enter minutes:");
+    scanf("%d", &min);
+    printf("Minutes = %d\n", min);
+    printf("Hours = %d\n",min/60);
+    printf("Days = %d\n",min/1440);
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
