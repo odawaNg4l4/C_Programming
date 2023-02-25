c76dfcc40ddd502342fb7c77bfce49d3dce1d4ed
odawaNg4l4 <odawangala99@gmail.com>
2023-02-25 23:58:00 +0300

    'c'

--- Prime Numbers.c
+++ Prime Numbers.c
@@ -6,32 +6,30 @@ int main()
 {
     int p;
     int i;
-
-    int prime[50] = {0};
+    int primes[50] = {0};
     int primeIndex = 2;
+
     bool isPrime;
+    //HARD CODE NUMBERS
+    primes [0] = 2;
+    primes [1] = 3;
 
-    //Hardcode prime numbes
-    prime[0] = 2;
-    prime[1] = 3;
-    for (p=5 ; p<=100; p=p+2)
+    for(p = 5; p <=100; p = p + 2)
     {
-       isPrime = true;
+        isPrime = true;
 
-        for(i=1; isPrime && p / prime[i] >= prime[i] ; ++i)
+        for(i = 1; isPrime && p / primes[i] >= primes[i]; ++i)
+            if(p % primes[i] == 0)
+            isPrime = false;
+        if (isPrime == true)
         {
-            if (p %prime[i] == 0);
-                isPrime = false;
-            if(isPrime == true);
-                prime[primeIndex] = p;
-                ++primeIndex;
+            primes[primeIndex]=p;
+            ++primeIndex;
         }
-
-        for(i = 0; i <primeIndex; ++i)
-        printf("%i", prime[i]);
-
-        printf("\n");
     }
+    for(i = 0; i<primeIndex; ++i)
+    printf("%i", primes[i]);
 
+    printf("\n");
     return 0;
 }
