56aa51029f9d0535b945f6154aaad4405d1ea1ce
odawaNg4l4 <odawangala99@gmail.com>
2023-02-27 23:41:46 +0300

    'Functions'

---
+++ FUNCTIONS.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// Create a function
+void myFunction() {
+  printf("A goatie in the making");
+}
+
+int main() {
+  myFunction();
+  printf("\n"); // call the function
+  myFunction();
+  return 0;
+}
+
+
