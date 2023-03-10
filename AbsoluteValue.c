f0985b7e57a91c82929b3ce331960d28ece8d65c
odawaNg4l4 <odawangala99@gmail.com>
2023-03-10 17:50:54 +0300

    c

---
+++ AbsoluteValue.c
@@ -0,0 +1,32 @@
+/*#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int x,y;
+
+    x = abs(5);
+
+    y = abs(10);
+
+    printf("Absolute vlue of %d\n", x);
+    printf("Absolute value of %d\n", y);
+
+    return 0;
+}*/
+#include<stdio.h>
+#include<stdlib.h>
+     
+    int main()
+    {
+        int x, y;
+
+        printf("Enter a number to get its absolute value");
+        scanf("%d\n", &x);
+
+        y = abs(x);
+
+        printf("Absolute value of %d is %d\n", x , y);
+        return 0;
+    }    
+ 
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,16 @@
+/*#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int x,y;
+
+    x = abs(5);
+
+    y = abs(10);
+
+    printf("Absolute vlue of %d\n", x);
+    printf("Absolute value of %d\n", y);
+
+    return 0;
+}*/
