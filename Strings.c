af2bb78b7d585de90f1a92bc43e27e25a3185281
odawaNg4l4 <odawangala99@gmail.com>
2023-03-14 08:50:15 +0300

    c

--- Strings.c
+++ Strings.c
@@ -29,3 +29,16 @@ for (i = 0; i < 14; ++i)
   printf("%c\n", dogName[i]);
 }
 }
+
+#include<stdio.h>
+
+int main()
+{
+    char greetings [] = {'H','E','L','L','O','W','O','R','L','D','\0'};
+    char greetings1 [] = "Hello world";
+
+    printf("%lu\n",sizeof(greetings));
+    printf("%lu\n",sizeof(greetings1));
+
+    return 0;
+}
