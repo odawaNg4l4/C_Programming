aa2e0a4c735e8e170c916496bef04d1b17c1af6e
odawaNg4l4 <odawangala99@gmail.com>
2023-04-29 18:19:31 +0300

    c

--- User_input.c
+++ User_input.c
@@ -1,13 +1,23 @@
-#include<stdio.h>
+/*#include<stdio.h>
 
 char fullName[30];
 int main()
 {
     printf("Enter name of character");
     fgets(fullName, sizeof(fullName), stdin);
-   
-
     printf("You %s are like the stars", fullName);
 
     return 0;
+}*/
+
+#include<stdio.h>
+int myNum;
+char myChar;
+
+int main()
+{
+    printf("Enter age and letter");
+    scanf("%d %c", &myNum, &myChar);
+    printf("Your age is %d\n", myNum);
+    printf("Your letter is %c\n" , myChar);
 }
