5f1c2cf90f02b258ad06138949b92a719b368493
odawaNg4l4 <odawangala99@gmail.com>
2023-06-20 11:29:19 +0300

    C

--- Fibonacci_Sequence.c
+++ Fibonacci_Sequence.c
@@ -2,10 +2,16 @@
 #include<stdlib.h>
 int main()
 {
-   int i;
-   int x = 0;
-   for(i=0; i<=16; i++){
-      x = x + i;
-      printf("%d\n", x);
+   int i,n;
+   int t1 = 0, t2 =1;
+   int nextTerm = t1 + t2;
+   printf("Enter the number of terms:");
+   scanf("%d", &n);
+   printf("Fibonacci Series: %d, %d,", t1, t2);
+   for(i = 3; i<=n; ++i){
+      printf("%d,", nextTerm);
+      t1 = t2;
+      t2 = nextTerm;
+      nextTerm = t1 + t2;
    }
 }
